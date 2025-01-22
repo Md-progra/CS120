d11311dc10e443b48553b5056ef22e646310021a
Angel  Sarpong Antwi-Mensah <antwiangel77@gmail.com>
2025-01-22 16:42:12 +0000

    created a func ti find the digitalroots of numbers

---
+++ digitalroot4.py
@@ -0,0 +1,10 @@
+def digital_root(number):
+    while number >= 10:
+        sum = 0
+        for digit in str(number):
+            sum+=int(digit)
+        number = sum
+    print (number)
+    return number
+digital_root(2345)
+
