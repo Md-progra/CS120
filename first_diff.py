bbb45144f83eabdb0fa0de363d2e8d1acfea96a0
Angel  Sarpong Antwi-Mensah <antwiangel77@gmail.com>
2025-01-23 16:33:38 +0000

    modified the program to add more 'stuff' to shorter word to make the length the same using .ljust

--- first_diff.py
+++ first_diff.py
@@ -1,15 +1,18 @@
-def first_diff(str1,str2):
+def first_diff(str1, str2):
     if str1 == str2:
-        print (-1)
+        print(-1)
         return -1
-    for letter in range(min(len(str1), len(str2))):
-        if str1[letter] != str2[letter]:
-            print (letter)
-            return letter
-        else:
-            return (min(len(str1),len(str2)))
-    
-first_diff("man","mantle")
-       
-       
-    
+    if len(str1) != len(str2):
+        max_length = max(len(str1), len(str2))
+        str1 = str1.ljust(max_length)
+        str2 = str2.ljust(max_length)
+    for i in range(len(str1)):
+        if str1[i] != str2[i]:
+            print(i)
+            return i
+    print(-1)
+    return -1
+
+# Example usage
+first_diff('baccs', 'bac')
+first_diff('daddy','daddye')
