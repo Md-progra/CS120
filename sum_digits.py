200d4f7e8c952670095c0142375d0965da216e03
Angel  Sarpong Antwi-Mensah <antwiangel77@gmail.com>
2025-01-23 15:56:26 +0000

    modified in class

--- sum_digits.py
+++ sum_digits.py
@@ -1,9 +1,8 @@
-def sum_digits():
-    num = int(input("Enter number:"))
+def sum_digits(num):
     num = str(num)
     sum = 0
     for number in num:
         sum+= int(number)
     print(sum)
     return(sum)
-sum_digits()
+sum_digits(1234)
