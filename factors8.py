b0810bdfcc58e912d75721754120625dcfbf1706
Angel  Sarpong Antwi-Mensah <antwiangel77@gmail.com>
2025-01-28 14:23:48 +0000

    hhhh

--- factors8.py
+++ factors8.py
@@ -1,14 +1,10 @@
-def number_of_factors(n):
-    if n == 0 and n < 0:
-        raise ValueError("Zero has no factors")
-    count = 0
-    
+def factors(n):
+    if n == 0 or n <0:
+        raise ValueError(" has no factors")
+    factors_of_ = []
     for i in range(1,n+1):
         if n % i == 0:
-            count += 1
-    return count
+            factors_of_.append(i)
+    return factors_of_
             
-
-        
-
 
