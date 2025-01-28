69fa4877c273ccbc2245fc773eabaa1da50412a7
Angel  Sarpong Antwi-Mensah <antwiangel77@gmail.com>
2025-01-28 16:14:13 +0000

    added a question in random7.py

--- random7.py
+++ random7.py
@@ -6,3 +6,23 @@ def gen_erate(n):
     print(generated_number)
 
 gen_erate(3)
+
+
+
+
+
+
+# Title: Find the Largest Element Less Than or Equal to a Given Number
+
+# Description:
+
+# Given a list of integers L and an integer n, 
+# write a function closest that returns the largest 
+# element in L that is not larger than n. If all elements in 
+# L are larger than n, return None.
+
+# The list L will have at least one element.
+
+# The elements in L are not necessarily sorted.
+
+# The elements in L and the integer n are all integers.
